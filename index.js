bed28d8964ce2362fc9ffc9ea8d1723cd547c2d3
badr ghanbi <bghanbi50@gmail.com>
2022-11-13 20:30:34 +0100

    add more styles

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hello')
+
+
+let form =document.querySelector('form');
+let clearItemsBtn=document.querySelector('.clear-items');
+
+let input=document.querySelector('input[type=text]');
+
+
+console.log(input)
+
